8723e9492e0f3b4f45f760c7f12576586d756db1
pcerezo <pcerezo70@gmail.com>
2024-01-04 20:09:47 +0100

    Esqueleto básico formado

---
+++ app/src/main/java/com/pableras/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,21 @@
+package com.pableras.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pableras.horoscapp.R
+import com.pableras.horoscapp.databinding.FragmentHoroscopeBinding
+
+class HoroscopeFragment : Fragment() {
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
